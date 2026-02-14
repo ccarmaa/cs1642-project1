be4a36b58679f23bcd2cc56480d3d54b31124506
F5F0A0 <42.741.42@gmail.com>
2026-02-14 10:33:25 -0500

    feat: implement http_server1

--- http_server1.c
+++ http_server1.c
@@ -38,19 +38,75 @@ handle_connection(int sock)
         					"<h2>404 FILE NOT FOUND</h2>\n"
         					"</body></html>\n";
     
-	(void)notok_response;  // DELETE ME
-	(void)ok_response_f;   // DELETE ME
+	// (void)notok_response;  // DELETE ME
+	// (void)ok_response_f;   // DELETE ME
 
     /* first read loop -- get request and headers*/
+    ///
+    int len;
+    char buf[BUFSIZE];
+    if ((len = recv(sock, buf, sizeof(buf) - 1, 0)) <= 0)  {
+        perror("http_server1: recv error");
+        exit(-1);
+    }
     
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
+    /// telnet www.washington.edu 80
+    /// GET / HTTP/1.0
+    /// or
+    /// GET /index.html HTTP/1.0
+    char file_name[FILENAMESIZE];
+    sscanf(buf, "GET %s HTTP", file_name);
+
+/// do we need this?
+    if (file_name[0] == '/') {
+        memmove(file_name, file_name + 1, strlen(file_name));
+    }
 
     /* open and read the file */
+    ///
+    FILE *fptr;
+    fptr = fopen(file_name, "r");
+    if (fptr == NULL) {
+        send(sock, notok_response, strlen(notok_response), 0);
+        close(sock);
+        return -1;
+    }
 	
 	/* send response */
+    /// rewrite buf?
+    /// write to socket?
+    // send(sock, ok_response_f, strlen(ok_response_f), 0); // wrong
+    // send(sock, fptr, strlen(fptr), 0); // wrong
+    fseek(fptr, 0, SEEK_END);
+    long file_size = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+
+    char header[BUFSIZE];
+    snprintf(header, BUFSIZE, ok_response_f, file_size);
+
+    send(sock, header, strlen(header), 0);
+
+    int bytes_read;
+    while ((bytes_read = fread(buf, 1, BUFSIZE, fptr)) > 0) {
+        int sent = 0;
+        while (sent < bytes_read) {
+            int res = send(sock, buf + sent, bytes_read - sent, 0);
+            if (res <= 0) {
+                perror("http_server1: send error");
+                close(sock);
+                fclose(fptr);
+                return -1;
+            }
+            sent += res;
+        }
+    }
 
     /* close socket and free pointers */
+    ///
+    close(sock);
+    fclose(fptr);
 
 	return 0;
 }
@@ -62,6 +118,11 @@ main(int argc, char ** argv)
     int server_port = -1;
     int ret         =  0;
     int sock        = -1;
+
+    ///
+    int s;
+    struct sockaddr_in saddr;
+    ///
 
     /* parse command line args */
     if (argc != 2) {
@@ -77,18 +138,43 @@ main(int argc, char ** argv)
     }
 
     /* initialize and make socket */
+    ///
+    if ((s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) { 
+        perror("http_server1: socket error");
+        exit(-1);
+    }
 
     /* set server address */
+    ///
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_port = htons(server_port);
 
     /* bind listening socket */
+    ///
+    if (bind(s, (struct sockaddr *)&saddr, sizeof(saddr)) < 0) {
+        perror("http_server1: bind error");
+        exit(-1);
+    }
 
     /* start listening */
+    /// max queued connections = 32
+    if (listen(s, 32) < 0) {
+        perror("http_server1: listen error");
+        exit(-1);
+    }
 
     /* connection handling loop: wait to accept connection */
     while (1) {
         /* handle connections */
+        ///
+        sock = accept(s, NULL, NULL);
+        if (sock < 0) {
+            perror("http_server1: accept error");
+            continue;
+        }
         ret = handle_connection(sock);
-
 		(void)ret; // DELETE ME
     }
 }
