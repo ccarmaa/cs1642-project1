2e4396c24498bf95e064b7ad34fa55d7792d8c04
carmaa <crfrencho@gmail.com>
2026-02-13 20:32:36 -0500

    server 2 looks good. maybe...

--- http_server2.c
+++ http_server2.c
@@ -37,22 +37,110 @@ handle_connection(int sock)
                             "<h2>404 FILE NOT FOUND</h2>\n"				\
                             "</body></html>\n";
     
-  (void)notok_response;  // DELETE ME
-  (void)ok_response_f;   // DELETE ME
+  //(void)notok_response;  // DELETE ME
+  //(void)ok_response_f;   // DELETE ME
+    // ok this is literally the same as server1 :DDDD
+    // so im just pasting it in  
+
+    char buf[BUFSIZE];
+    memset(buf, 0, BUFSIZE); //clear buf
+    char filename[FILENAMESIZE];
+    char header[BUFSIZE];
+
+    char method[10], path[FILENAMESIZE], version[10];
+    FILE *file;
+    int len, bytes_read, file_size;
 
     /* first read loop -- get request and headers*/
+    // from tcp_server.c
 
+
+    /* uh im concerned that we need to loop until we see /n/n/n/n or whatever so im gonna save this incase i fail miserably :D
+    if ((len = recv(sock, buf, BUFSIZE - 1, 0)) <= 0) {
+        perror("http_server1: recv error");
+        return -1;
+    }
+    buf[len] = '\0'; // null term so we can print it
+    */
+    int total_len = 0;
+    while(total_len < BUFSIZE -1){
+        /*
+            here me out this means:
+            - start writing at buf+total_len (so we dont overwrite)
+            - write at most BUFSIZE - 1 - total_len bytes (so we dont overflow)
+        */
+
+        if((len = recv(sock, buf + total_len, BUFSIZE - 1 - total_len, 0)) <= 0) {
+            perror("http_server1: recv error");
+            return -1;
+        }
+        total_len += len;
+        buf[total_len] = '\0';
+
+        // ok if our thing has the /n/n/n/n stuff we have the full thing, so we can break
+        if (strstr(buf, "\r\n\r\n") != NULL) {
+            break;
+        }
+    }
+
+    
+    
     /* parse request to get file name */
+    /* Assumption: this is a GET request and filename contains no spaces*/
+    if (sscanf(buf, "%s %s %s", method, path, version) != 3) {
+        fprintf(stderr, "http_server1: failed to parse request\n");
+        return -1;
+    }
 
-    /* Assumption: For this project you only need to handle GET requests and filenames that contain no spaces */
-  
+    // extract filename from path
+    // skip leading '/' if there is one
+    if (path[0] == '/') {
+        strncpy(filename, path + 1, FILENAMESIZE - 1);
+    } else {
+        strncpy(filename, path, FILENAMESIZE - 1);
+    }
+    filename[FILENAMESIZE - 1] = '\0'; // ensure null term
+
     /* open and read the file */
-  
-    /* send response */
+	if ((file = fopen(filename, "rb")) == NULL) {
+    
+    // send 404 if not found
+        if (send(sock, notok_response, strlen(notok_response), 0) <= 0) {
+            perror("http_server1: send error");
+            return -1;
+        }
+        return 0;
+    }
+	
+    // resource: https://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
+    // get the file size 
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
 
-    /* close socket and free space */
-  
-    return 0;
+    /* send response */ 
+    // send header
+    sprintf(header, ok_response_f, file_size); // sprintf(destbuff, format str, size)
+    // basically same as tcp  server. but we are sending header rn
+    if (send(sock, header, strlen(header), 0) <= 0) {
+        perror("http_server1: send error");
+        fclose(file);
+        return -1;
+    }
+    // send file contents
+    // kinda just keeps reading and sending til there aint more to read
+    while ((bytes_read = fread(buf, 1, BUFSIZE, file)) > 0) {
+        if (send(sock, buf, bytes_read, 0) <= 0) {
+            perror("http_server1: send error");
+            fclose(file);
+            return -1;
+        }
+    }
+    
+    /* close socket and free pointers */ // no im doing that not in here
+    fclose(file);
+	return 0;
 }
 
 
@@ -78,28 +166,124 @@ main(int argc, char ** argv)
     }
     
     /* initialize and make socket */
-
+    // same as tcp_server.c
+    if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("tcp_server: socket error");
+        exit(-1);
+    }
     /* set server address */
+    struct sockaddr_in saddr;
 
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_port = htons(server_port);
     /* bind listening socket */
-
+    if (bind(sock, (struct sockaddr *)&saddr, sizeof(saddr)) < 0) {
+        perror("tcp_server: bind error");
+        exit(-1);
+    }
     /* start listening */
-
+    if (listen(sock, 32) < 0) {
+        perror("tcp_server: listen error");
+        exit(-1);
+    }
     /* connection handling loop: wait to accept connection */
+
+    int connections[32];
+    int max_fd; // for select
+    int ready; // for select (how many sockets are ready)
+
+    fd_set read_fds, all_fds;
+
+    // initialize connections array to -1 (aka empty)
+    for (int i = 0; i < 32; i++) {
+        connections[i] = -1;
+    }
+
+    FD_ZERO(&all_fds);
+    FD_SET(sock, &all_fds); 
+    max_fd = sock;
 
     while (1) {
     
         /* create read list */
+        read_fds = all_fds;
+        /*
+            ok so the reason we need read_fds and all_fds is because select modifies 
+            the set you pass in to indicate which ones are ready
+            
+            so we keep an unmodified copy in all_fds and copy it to read_fds each time 
+            before we call select :D
+        */
         
         /* do a select() */
-        
+        // resource: https://stackoverflow.com/questions/32711521/how-to-use-select-on-sockets-properly
+        // & i did something similar in the client
+        ready = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
+
+        if (ready < 0) {
+            perror("http_server2: select error");
+            exit(-1);
+        }
         /* process sockets that are ready:
-         *     for the accept socket, add accepted connection to connections
-         *     for a connection socket, handle the connection
+         *     for the accept socket, add accepted connection to connections PART 1
+         *     for a connection socket, handle the connection PART 2
          */
         
-        ret = handle_connection(sock);
+         //PART 1 
+        // check if listening socket is ready (aka new connection)
+        if (FD_ISSET(sock, &read_fds)) {
+            int new_conn = accept(sock, NULL, NULL);
+
+            if (new_conn < 0) {
+                perror("http_server2: accept error");
+                exit(-1);
+            }else{
+                //find empty space
+                int added = 0;
+                for(int i = 0; i < 32; i++) {
+                    if (connections[i] == -1) {
+                        connections[i] = new_conn;
+                        FD_SET(new_conn, &all_fds);
+                        if (new_conn > max_fd) {
+                            max_fd = new_conn;
+                        }
+                        printf("New connection accepted: %d\n", new_conn);
+                        added = 1;
+                        break;
+                    }
+                }
+                if (!added) {
+                    fprintf(stderr, "Too many connections, rejecting new connection\n");
+                    close(new_conn);
+                }
+            }
+        }
+
+        // PARRT2
+        // check all conn sockets
+        for(int i=0; i < 32; i++) {
+            int conn = connections[i];
+
+            if(conn == -1) {
+                continue; //empty
+            }
+
+            if (FD_ISSET(conn, &read_fds)) {
+                printf("Handling connection: %d\n", conn);
+                ret = handle_connection(conn);
+
+                close(conn);
+                FD_CLR(conn, &all_fds); // remove from set
+                connections[i] = -1; // mark as empty
+            }
+        }
+
+        //ret = handle_connection(sock);
         
-        (void)ret;  // DELETE ME
+        //(void)ret;  // DELETE ME
     }
+    close(sock);
+    return 0;
 }
